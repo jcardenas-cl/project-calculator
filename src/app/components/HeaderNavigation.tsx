d4c65aabfa421305cd90cd29b523d4a4572b034e
Julio <julio.cardenas.gatica@gmail.com>
2025-02-26 19:42:34 -0300

    Base limpia

---
+++ src/app/components/HeaderNavigation.tsx
@@ -0,0 +1,12 @@
+export default function HeaderNavigation() {
+    return (
+        <nav>
+            <div className="bg-slate-700 text-white justify-evenly">
+                <div className="flex container m-auto py-5">
+                    <div className="w-1/3">[LOGO]</div>
+                    <div className="w-2/3 text-right">Inicio - Mis Proyectos - Contacto</div>
+                </div>
+            </div>
+        </nav>
+    )
+}
